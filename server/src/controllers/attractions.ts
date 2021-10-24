daab42b409b221525e65318540fc528a44e9c107
david <dudiko2@gmail.com>
2021-10-24 12:20:44 +0300

    reformat controller & service

--- server/src/controllers/attractions.ts
+++ server/src/controllers/attractions.ts
@@ -1,6 +1,5 @@
 import { Handler } from "express";
-import { distanceBetweenCoordinates } from "../lib/utils";
-import { getAttractions } from "../services/attractions";
+import { getAttractions, formatAttractions } from "../services/attractions";
 
 export const getNearestAttractions: Handler = async (req, res) => {
     const { lat, long } = req.query;
@@ -9,33 +8,20 @@ export const getNearestAttractions: Handler = async (req, res) => {
         return res.status(400).json({ msg: "missing parameters" });
     }
 
+    const latitude = lat.toString();
+    const longitude = long.toString();
+
     const [attractions, error] = await getAttractions();
 
     if (error) {
         return res.status(500).json({ msg: "server error" });
     }
 
-    // get compare function
-    const distanceFromUser = distanceBetweenCoordinates({
-        latitude: parseFloat(lat as string),
-        longitude: parseFloat(long as string)
-    });
-
-    //send relevant entries
     const result = attractions
-        ?.map((a) => {
-            const attraction = a.toJSON();
-
-            const { lat: aLat, long: aLong } = attraction;
-            const distance = distanceFromUser({
-                latitude: aLat,
-                longitude: aLong
-            });
-
-            return { ...attraction, distance };
-        })
-        .sort((a, b) => a.distance - b.distance)
-        .filter((a) => a.distance <= 40);
+        ? formatAttractions({ attractions, lat: latitude, long: longitude })
+              .sort((a, b) => a.distance - b.distance)
+              .filter((a) => a.distance <= 40)
+        : [];
 
     return res.json(result);
 };
--- server/src/services/attractions.ts
+++ server/src/services/attractions.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import { IAttraction } from "../db";
-import { tryCatch } from "../lib/utils";
+import { tryCatch, distanceBetweenCoordinates } from "../lib/utils";
 
 export const getAttractions = async () => {
     const Attraction = mongoose.model<IAttraction>("Attraction");
@@ -10,4 +10,37 @@ export const getAttractions = async () => {
     );
 
     return [attractions, error] as const;
+};
+
+interface FormatAttractionsArgs {
+    attractions: Array<
+        mongoose.Document<unknown, unknown, IAttraction> & IAttraction
+    >;
+    lat: string;
+    long: string;
+}
+
+type FormatAttractionsReturnType = Array<IAttraction & { distance: number }>;
+
+export const formatAttractions = ({
+    attractions,
+    lat,
+    long
+}: FormatAttractionsArgs): FormatAttractionsReturnType => {
+    const distanceFromUser = distanceBetweenCoordinates({
+        latitude: parseFloat(lat),
+        longitude: parseFloat(long)
+    });
+
+    return attractions.map((a) => {
+        const attraction = a.toJSON();
+
+        const { lat: aLat, long: aLong } = attraction;
+        const distance = distanceFromUser({
+            latitude: aLat,
+            longitude: aLong
+        });
+
+        return { ...attraction, distance };
+    });
 };
